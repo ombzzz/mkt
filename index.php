be0baa46ef3601311ece0be52d268e325577690d
obiazzi@gmail.com <obiazzi@gmail.com>
2018-02-13 00:10:26 +0000

    avance

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <html>
+<style>
+.log {
+	white-space: pre-wrap;
+}
+.error {
+	color: red;
+}
+</style>
 <?
 
 main();
@@ -23,11 +31,11 @@ function main(){
 	$last = $_POST["last"];
 	$pers = $_POST["pers"];
 	$upd = $_POST["upd"];
+	$updall = $_POST["updall"];
 
 	if( $login == "login" ){
 		$json = login( $usr, $pas );
 		$jsona = json_decode( $json, true );
-		//var_dump( $jsona );
 		$bearer = $jsona["access_token"];
 	}
 	if( $cotiz == "cotiz" ){
@@ -35,30 +43,7 @@ function main(){
 		$cotiza = json_decode( $cotizx, true );
 		$titulos = $cotiza["titulos"];
 
-		//{
-		// "simbolo": "AGRO",
-		//  "puntas": {
-		//  	 "cantidadCompra": 0.0,
-		//  	 "precioCompra": 0.000,
-		//  	 "precioVenta": 25.500,
-		//  	 "cantidadVenta": 22.0 
-		//  },
-		//  "ultimoPrecio": 22.500,
-		//  "variacionPorcentual": -1.74,
-		//  "apertura": 22.600,
-		//  "maximo": 23.400, 
-		//  "minimo": 21.500,
-		//  "ultimoCierre": 22.500,
-		//  "volumen": 0.0,
-		//  "cantidadOperaciones": 242.0,
-		//  "fecha": "2018-02-09T17:00:06.503", 
-		//  "tipoOpcion": null,
-		//  "precioEjercicio": null,
-		//  "fechaVencimiento": null,
-		//  "mercado": "BCBA",
-		//  "moneda": "AR$"
-		// }
-
+		//formato del json en la funcion c-otiz
 		foreach( $titulos as $key => $value ){
 			echo "<div>".$value["simbolo"]."</div>";
 		}
@@ -82,6 +67,9 @@ function main(){
 	if( $upd == "upd" ){
 		upd( $tck, $bearer );
 	}
+	if( $updall == "updall" ){
+		updall( $bearer );
+	}
 
 	form( $usr, $pas, $panel, $cotiz, $tck, $de, $a, $bearer );
 }
@@ -103,40 +91,120 @@ function fecha_hoy(){
 }
 
 //------------------------------------------------
+// updall
+//------------------------------------------------
+function updall( $bearer ){
+	loguear( "updall invocado" );
+	$cant = 0;
+
+	$asx = assets( 'bcba' );
+
+	foreach( $asx as $as ){
+		$res = upd( $as, $bearer );
+		if( $res == 0 )
+			$cant = $cant + 1;
+	}
+	$pdo = null; //para close connection 
+	$pds = null; //para close connection
+	loguear( "updall finalizado, $cant assets actualizados" );
+	return;
+
+}
+
+
+//------------------------------------------------
+// assets
+//------------------------------------------------
+function assets( $mkt ){
+	$database = 'mkt';
+	$dsn = "sqlite:$database.db";
+
+	try {
+		$pdo = new PDO( $dsn ); // sqlite
+	} catch( PDOException $e ) {
+		loguear( "updall: error en conn: " . $e->getMessage(), "error" );
+		return;
+	}
+
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	//pds es un objeto pdostatement , que define interfaz iterable, por eso funciona el foreach 
+	try {
+		$pds = $pdo->query( "select tck, mkt from asset a where a.mkt = 'bcba'" );
+	} catch( PDOException $e ) {
+		loguear( "updall: error en sel: " . $e->getMessage(), "error" );
+		return;
+	}
+
+	$i = 0;
+	foreach( $pds as $row )
+		$res[$i++] = $row[ "tck" ];
+
+	$pdo = null; //para close connection 
+	$pds = null; //para close connection
+	
+	return $res;
+}
+
+//------------------------------------------------
 // upd
 //------------------------------------------------
 function upd( $tck, $bearer ){
+	loguear( "upd: invocado para $tck" );
 	$lp = last( $tck );
 	if( $lp == null ){
-		echo "<p>$tck no tiene precios registrados";
+		loguear( "upd $tck: sin precios registrados => se buscaran desde 3/12/2001" );
 		$de = "2001-12-03";
 	}
 	else {
-		echo "<p>ultimo precio de $tck fue " . $lp;
+		loguear( "upd $tck: ult precio " . $lp );
 		$de = fecha_offset( $lp, 1 ); 
 	}
 	$a = fecha_hoy();
-	echo "<p>llamando a pers de $de a $a para $tck</p>";
+	if( $lp == $a ){
+		loguear( "upd $tck: up to date => NOP" );
+		return 0;
+	}
 	$cant = pers( $tck, $de, $a, $bearer );
-	echo "<p>actualizados $cant precios de $tck de $de a $a</p>";
+	if( $cant >= 0 ){
+		loguear( "upd $tck: $cant precios registrados" );
+		return 0;
+	}
+	else {
+		loguear( "upd $tck: problemas registrando precios", "error" );
+		return 1;
+	}
 }
 
 //------------------------------------------------
 // pers
 //------------------------------------------------
 function pers( $tck, $de, $a, $bearer ){
+	loguear( "pers: invocado para $tck de $de a $a" );
 
 	$cant = 0;
 	$preciosx = precios( $tck, $de, $a, $bearer );
 	$preciosa = json_decode( $preciosx, true );
+	if( count( $preciosa ) == 1 && key( $preciosa ) == "message" ){
+		loguear( "pers $tck $de $a: problemas en get de precios: " . key( $preciosa ) . "->" . $preciosa["message"], "error" );
+		return -1;
+	}
+	else if( $preciosa[0]["fechaHora"] > 0 ){
+	 loguear( "pers $tck $de $a: precios obtenidos, recorriendo" );
+	}
 
 	$database = 'mkt';
 	$dsn = "sqlite:$database.db";
 	try {
 		$pdo = new PDO( $dsn ); // sqlite
 	} catch( PDOException $e ) {
-		die ( 'Oops' . $e ); // Exit, displaying an error message
+		loguear( "pers: error en conn: " . $e->getMessage(), "error" );
+		return;
 	}
+
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $pdo->beginTransaction();
 
 	foreach( $preciosa as $key => $value ){
 		$tck = strtolower( $tck );
@@ -161,16 +229,32 @@ function pers( $tck, $de, $a, $bearer ){
 		)
 FINN;
 
-		//echo $sql . "<br>";
-		$rowcount = $pdo->exec( $sql );
-		if( ! $rowcount > 0 ){
-			print_r( $pdo->errorInfo() ) . "<br>";
+		try {
+			$pdo->exec( $sql );
+		} catch( PDOException $e ) {
+			$pdo->rollback();
+			loguear( "pers: error en ins: " . $e->getMessage() . " sql $sql", "error" );
+			return -1;
 		}
-		else {
-			$cant = $cant + 1;
-		}
+
+		$cant = $cant + 1;
+		if( $cant%100 == 0 )
+			loguear( "pers $tck $de $a: van $cant" );
 	}
-	
+	loguear( "pers $tck $de $a: fuera de cursor" );
+
+	try {
+		$pdo->commit();
+	} catch( PDOException $e ) {
+		loguear( "pers $tck $de $a: error en c-ommit: " . $e->getMessage(), "error" );
+		return -1;
+	}
+
+	loguear( "pers $tck: --commit--" );
+
+	$pdo = null; //para close connection 
+	$pds = null; //para close connection
+
 	return $cant;
 
 	/*
@@ -205,36 +289,19 @@ function last( $tck ){
 	try {
 		$pdo = new PDO( $dsn ); // sqlite
 	} catch( PDOException $e ) {
-		die ( 'Oops' . $e ); // Exit, displaying an error message
+		loguear( "last $tck: error " . $e->getMessage(), "error" );
+		return;
 	}
 
 	$tck = strtolower( $tck );
 	//pds es un objeto pdostatement , que define interfaz iterable, por eso funciona el foreach 
 	$pds = $pdo->query( "select max( fec ) fec from as_pr where tck = '" . $tck. "'" );
 	$row = $pds->fetch();
+
+	$pdo = null; //para close connection 
+	$pds = null; //para close connection
+
 	return $row[ "fec" ];
-}
-
-
-//------------------------------------------------
-// sqlite
-//------------------------------------------------
-function sqlite(){
-
-	$database = 'mkt';
-	$dsn = "sqlite:$database.db";
-
-	try {
-		$pdo = new PDO( $dsn ); // sqlite
-	} catch( PDOException $e ) {
-		die ( 'Oops' . $e ); // Exit, displaying an error message
-	}
-
-	//pds es un objeto pdostatement , que define interfaz iterable, por eso funciona el foreach 
-	$pds = $pdo->query( "select tck from asset" );
-	foreach( $pds as $row ){
-		echo "<div>" . $row[ "tck" ] . "</div>";
-	}
 }
 
 //------------------------------------------------
@@ -242,10 +309,11 @@ function sqlite(){
 //------------------------------------------------
 function precios( $tck, $de, $a, $bearer ){
 	$tck = strtoupper($tck);
+
 	//GET /api/{mercado}/Titulos/{simbolo}/Cotizacion/seriehistorica/{fechaDesde}/{fechaHasta}/{ajustada}
 	//mercado: bCBA, nYSE, nASDAQ, aMEX, bCS, rOFX
 	$url = "https://api.invertironline.com/api/bCBA/Titulos/$tck/Cotizacion/seriehistorica/$de/$a/ajustada";
-	echo "<p>precios: $url</p>";
+	loguear( "precios: $url" );
 
 	$ch = curl_init();
 	curl_setopt( $ch, CURLOPT_URL, $url );
@@ -253,6 +321,7 @@ function precios( $tck, $de, $a, $bearer ){
 	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
 	$server_output = curl_exec( $ch );
 	curl_close( $ch );
+	loguear( "precios: volvio curl, devolviendo precios" );
   return $server_output;
 }
 
@@ -281,6 +350,30 @@ function cotiz( $bearer, $panel ){
 	$server_output = curl_exec( $ch );
 	curl_close( $ch );
   return $server_output;
+
+	//{
+	// "simbolo": "AGRO",
+	//  "puntas": {
+	//  	 "cantidadCompra": 0.0,
+	//  	 "precioCompra": 0.000,
+	//  	 "precioVenta": 25.500,
+	//  	 "cantidadVenta": 22.0 
+	//  },
+	//  "ultimoPrecio": 22.500,
+	//  "variacionPorcentual": -1.74,
+	//  "apertura": 22.600,
+	//  "maximo": 23.400, 
+	//  "minimo": 21.500,
+	//  "ultimoCierre": 22.500,
+	//  "volumen": 0.0,
+	//  "cantidadOperaciones": 242.0,
+	//  "fecha": "2018-02-09T17:00:06.503", 
+	//  "tipoOpcion": null,
+	//  "precioEjercicio": null,
+	//  "fechaVencimiento": null,
+	//  "mercado": "BCBA",
+	//  "moneda": "AR$"
+	// }
 
 }
 
@@ -315,6 +408,7 @@ function form( $usr, $pas, $panel, $cotiz, $tck, $de, $a, $bearer ){
 	<input type=submit name=last value=last>
 	<input type=submit name=pers value=pers>
 	<input type=submit name=upd value=upd>
+	<input type=submit name=updall value=updall>
 	<br>
 	<textarea cols=100 name=bearer>$bearer</textarea>
 	<hr>
@@ -344,6 +438,85 @@ function login( $usr, $pas ){
 
   return $server_output;
 
+}
+
+
+//------------------------------------------------
+// loguear
+//------------------------------------------------
+function loguear( $que, $tipo = "inform", $display = 1, $rendlog = 0, $nomail = 0 ){
+
+	$filename = "mkt.log";
+	if( !$fp = fopen($filename, 'a+' ) ){
+		echo "no se puede abrir w+ el archivo $fileName";
+	}
+	else {
+		$fecha = date( "Ymd H:i:s" );
+
+		fwrite( $fp, "$fecha $que\n" );
+		if( $display ){
+			$cls = "log $tipo";
+			echo "<div class=' " . $cls . "'>$que</div>";
+		}
+		fclose( $fp );
+	}
+
+	if( $tipo == "error" )
+		$error = 1;
+	else
+		$error = 0;
+
+	if( $rendlog ){
+		log_insertar( "$fecha $que", $error );
+	}
+
+	if( $tipo == "error" && ! $nomail ){
+		$ret = reportar( "dms.gti@ecogas.com.ar", "obiazzi@ecogas.com.ar", "$fecha $que", "rend.php " . substr( $que, 0, 50 ) );
+		if( $ret != "ok" ){
+			//recursivo pero sin envio de mail ya que esta fallanado
+			//por seguridad recursividad hacemos s-leep
+			$sleepsec = 60;
+			sleep($sleepsec);
+			//        c-encuyx, q-ue,                                                  t-ipo  $d-isp, l-og  n-omail
+			loguear( "rend.loguear: envio de mails no volvio ok sino $ret", "error", 1,     1,    1 );
+		}
+	}
+
+	return;
+}
+
+//------------------------------------------------
+// log_insertar
+//------------------------------------------------
+function log_insertar( $que, $error ){
+	return;
+}
+
+function reportar( $de, $a, $subj, $que ){
+	return "ok";
+}
+
+//------------------------------------------------
+// sqlite
+//------------------------------------------------
+function sqlite(){
+
+	$database = 'mkt';
+	$dsn = "sqlite:$database.db";
+
+	try {
+		$pdo = new PDO( $dsn ); // sqlite
+	} catch( PDOException $e ) {
+		die ( 'Oops' . $e->getMessage() ); // Exit, displaying an error message
+	}
+
+	//pds es un objeto pdostatement , que define interfaz iterable, por eso funciona el foreach 
+	$pds = $pdo->query( "select tck from asset" );
+	foreach( $pds as $row ){
+		echo "<div>" . $row[ "tck" ] . "</div>";
+	}
+	$pdo = null; //para close connection 
+	$pds = null; //para close connection
 }
 
 ?>
